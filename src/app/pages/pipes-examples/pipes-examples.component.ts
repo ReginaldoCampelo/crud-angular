042d8d83ef1f7531604649e30acf7e5d62e2998c
Reginaldo Campelo <regin.developer@gmail.com>
2022-02-20 22:19:23 -0300

    Implementando Pipes

--- src/app/pages/pipes-examples/pipes-examples.component.ts
+++ src/app/pages/pipes-examples/pipes-examples.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pipes-examples.component.css']
 })
 export class PipesExamplesComponent implements OnInit {
+  number = 0;
+  text = 'OI, Eu sOu GoKu!';
+  date = new Date;
+  pessoa = {
+    nome: 'Reginaldo',
+    idade: 28,
+    profissao: 'Programador'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mudarValor(){
+    this.number = 125.5888
+  }
+
+  mudarText(){
+    this.text = "eU nÃo SoU GokU!"
+  }
+
+  mudarJson(){
+    this.pessoa.nome = "Goku",
+    this.pessoa.idade = 5000
+    this.pessoa.profissao = "Lutador"
   }
 
 }
