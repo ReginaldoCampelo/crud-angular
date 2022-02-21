cbdc3c01c9d90aea5a16fff2906a5afe736b1b8d
Reginaldo Campelo <regin.developer@gmail.com>
2022-02-21 20:53:19 -0300

    Implementando Função Todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DataBindingComponent } from './pages/data-binding/data-binding.component';
 import { PipesExamplesComponent } from './pages/pipes-examples/pipes-examples.component';
 import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { TodoItemComponent } from './pages/todo/todo-item/todo-item.component'
+import { MatListModule } from '@angular/material/list'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -20,14 +28,22 @@ import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
     UserFormComponent,
     DataBindingComponent,
     PipesExamplesComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatGridListModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/todo/todo-list/todo-list.component.ts
+++ src/app/pages/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/model/todo';
 
 @Component({
   selector: 'crud-todo-list',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: Array<Todo> = [];
+  todo: Todo = {
+    id: 0,
+    title: '',
+    done: false
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
+    let items: any = localStorage.getItem('todos');
+    let todos = JSON.parse(items);
+    if(!todos) {
+      this.todos = [];
+    }else{
+      this.todos = todos;
+    }
+  }
+
+  addTodo(title: string){
+    const id = this.todos.length + 1;
+    this.todos.push({
+      id: id,
+      title: title,
+      done: false
+    });
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  removeTodo(todo: any){
+    let index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
 }
+
+
